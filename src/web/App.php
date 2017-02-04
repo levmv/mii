dda1919d05f952fcac25d9761316188ca1ec8032
levmorozov <mail@levmorozov.ru>
2017-02-04 23:42:22 +0300

    maintenance mode

--- src/web/App.php
+++ src/web/App.php
@@ -21,8 +21,27 @@ class App extends \mii\core\App
 {
     public $user;
 
+    public $maintenance;
+    public $maintenance_message;
+
     public function run()
     {
+
+        if($this->maintenance) {
+            $protocol = "HTTP/1.0";
+            if ( "HTTP/1.1" === $_SERVER["SERVER_PROTOCOL"] )
+                $protocol = "HTTP/1.1";
+            header( "$protocol 503 Service Unavailable", true, 503 );
+            header( "Retry-After: 30" );
+
+            echo $this->maintenance_message
+            ? $this->maintenance_message
+            : 'На сайте технические работы, которые закончатся через несколько секунд. Пожалуйста, обновите страницу в браузере';
+
+            die;
+        }
+
+
         $this->request->execute()->send();
     }
 
