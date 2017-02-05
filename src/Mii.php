07dd9f07d05c2f0c18487fd062328a8210650a87
levmorozov <mail@levmorozov.ru>
2017-02-06 01:16:26 +0300

    version up

--- src/Mii.php
+++ src/Mii.php
@@ -9,7 +9,7 @@ defined('MII_START_MEMORY') or define('MII_START_MEMORY', memory_get_usage());
 
 class Mii {
 
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     const CODENAME = 'Alpha Centauri';
 
