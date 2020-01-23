9fb156309b92a1f8106362d6c98d6fc30931f0e7
lev <mail@levmorozov.ru>
2020-01-23 18:30:24 +0300

    Fixed wrong call to parent class in forbidden exception

--- src/web/ForbiddenHttpException.php
+++ src/web/ForbiddenHttpException.php
@@ -5,8 +5,11 @@ namespace mii\web;
 class ForbiddenHttpException extends HttpException
 {
 
-    public function __construct($message = null, $variables = []) {
-        parent::__construct(403, $message, $variables);
+    public function __construct($message = null) {
+        if ($message === null) {
+            $message = "Access to ".Mii::$app->request->uri()." denied";
+        }
+        parent::__construct(403, $message);
     }
 
 }
