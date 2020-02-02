d1023387a70f27e194461fc459b1a5a76be1a58e
levmorozov <mail@levmorozov.ru>
2020-02-02 21:49:44 +0300

    Reverted wrong changes

--- src/db/ORM.php
+++ src/db/ORM.php
@@ -274,9 +274,6 @@ class ORM implements \JsonSerializable
             return \count($this->_changed) > 0;
         }
 
-        if(!\array_key_exists($field_name, $this->attributes))
-            return false;
-
         if (\is_array($field_name)) {
             return \count(\array_intersect($field_name, \array_keys($this->_changed)));
         }
