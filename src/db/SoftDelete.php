cb4f142507a096de3f6a7b45a23c38d352d0c0f0
lev <mail@levmorozov.ru>
2020-04-02 14:36:07 +0300

    Fix incorrect find_deleted method

--- src/db/SoftDelete.php
+++ src/db/SoftDelete.php
@@ -16,7 +16,20 @@ trait SoftDelete
 
     public static function find_deleted()
     {
-        return parent::select_query()->where('deleted', 'is not', null);
+        $model = (new static);
+
+        $query = $model->raw_query()->select(['`' . $model->get_table() . '`.*'], true)
+            ->from($model->get_table())
+            ->as_object(static::class, [null, true])
+            ->where('deleted', 'is not', null);
+
+        if ($model->order_by) {
+            foreach ($model->order_by as $column => $direction) {
+                $query->order_by($column, $direction);
+            }
+        }
+
+        return $query;
     }
 
     public function restore()
